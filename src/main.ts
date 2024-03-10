4ab6bb48be6ce2d4a5c6fdc522023a1591368964
Mikita <anykey2solo@gmail.com>
2024-03-11 00:40:39 +0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = await readFile(join(__dirname, '../doc/api.yaml'), 'utf-8');
+  const swaggerDocument = parse(file);
+
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
   await app.listen(4000);
 }
 bootstrap();
