ebb128ffe99911d7877818614adf2a5afe527a2a
Mikita <anyket2solo@gmail.com>
2024-03-24 19:08:17 +0300

    feat: add migration file and migration run

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -31,9 +32,13 @@ import { FavoritesModule } from './favorites/favorites.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
-        synchronize: true,
+        synchronize: false,
       }),
       inject: [ConfigService],
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
   ],
   controllers: [AppController],
